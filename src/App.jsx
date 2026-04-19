96923c8c328bfc6061a78b82f1a7f2fcdc3ecd84
kiendt298 <kiendt298@gmail.com>
2026-04-20 01:56:03 +0700

    .

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ const ENV_CONFIG = {
     // Dịch env về phía âm X để phòng tròn lớn (bên trái trong layout)
     // trở thành trung tâm của scene
     centerOffset: { x: -8, z: 0 },
-    cameraPos:    { x: -15, y: 1.6, z:  0 },  // gần nhất: x: 15, y: 1.6, z:  5 
+    cameraPos:    { x: -15, y: 1.6, z:  -10 },  // gần nhất: x: 15, y: 1.6, z:  5 
     cameraTarget: { x: -25, y: 1.4, z: -3 },  // gần nhất:  x: -20, y: 1.4, z: -3
     modelPos:     { x: -15, y: 0, z: 0 },  // x: -10, y: 0, z: 0
     modelRotY:    20                         // quay mặt về z+ (về phía camera)        
