9804bda7b53f3d9b210936d89cd5e435a5b17781
kiendt298 <kiendt298@gmail.com>
2026-04-20 11:48:54 +0700

    .

--- src/App.jsx
+++ src/App.jsx
@@ -37,9 +37,9 @@ const ENV_CONFIG = {
     // Dịch env về phía âm X để phòng tròn lớn (bên trái trong layout)
     // trở thành trung tâm của scene
     centerOffset: { x: -10, z: 0 }, // khoảng cách camera với model
-    cameraPos:    { x: -5, y: 1.6, z:  0 },  // tiến lùi của camrera
+    cameraPos:    { x: -10, y: 1.6, z:  0 },  // tiến lùi của camrera
     cameraTarget: { x: -25, y: 1.4, z: 0 }, // hướng của camrera
-    modelPos:     { x: -35, y: 0, z: 0 }, 
+    modelPos:     { x: -35, y: 0, z: 0 }, // vị trí của model
     modelRotY:    14.2                         // model quay mặt về phía camera       
   },
 }
